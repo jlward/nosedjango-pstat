3ecc6cad4102229dae1346aea6d8b3071acc67fa
Jason Ward <jason.ward@policystat.com>
2011-03-15 13:42:05 -0400

    compleated the pstat plugin

--- pstat_plugin.py
+++ pstat_plugin.py
@@ -1,5 +1,36 @@
-from nose.plugins import Plugin
+import string
+import math
+import random
+import logging
+
+from nosedjango.plugins.base_plugin import Plugin
 
 class PstatPlugin(Plugin):
 
     name = 'pstat'
+
+    def beforeTestSetup(self, settings, setup_test_environment, connection):
+        logging.getLogger().setLevel(logging.WARNING)
+        switched_settings = {
+            'DOCUMENT_IMPORT_STORAGE_DIR': 'document_import%(token)s',
+            'DOCUMENT_SETTINGS_STORAGE_DIR': 'document_settings%(token)s',
+            'ATTACHMENT_STORAGE_PREFIX': 'attachments%(token)s',
+            'MAILER_LOCKFILE': 'send_mail%(token)s',
+        }
+        settings.DOCUMENT_PRINTING_CACHE_ON_SAVE = False
+
+        from pstat.printing.conf import settings as print_settings
+        from pstat.document_backup.conf import settings as backup_settings
+        token = self.get_unique_token()
+        print_settings.PDF_STORAGE_DIR = 'unittest/pdf_cache%s/' % token
+        print_settings.PDF_STORAGE_BASE_URL = 'unittest/pdf_cache%s/' % token
+        backup_settings.STORAGE_DIR = 'unittest/document_backup%s/' % token
+        backup_settings.STORAGE_BASE_URL = 'unittest/document_backup%s/' % token
+
+        for key, value in switched_settings.items():
+            setattr(settings, key, value % {'token': token})
+        settings.CACHE_BACKEND = 'locmem://'
+        settings.DISABLE_QUERYSET_CACHE = True
+
+        from django.core.cache import cache
+        cache.clear()
